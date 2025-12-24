c4e1c7c2f54293d8214d99fa5f6ff9bf8aa839d8
Avotraina <avotrainarabe@gmail.com>
2025-12-24 14:40:59 +0300

    feature[settings]: Refactor DrawerLayout to enhance theming and improve back button styling

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,9 +1,12 @@
 import { useRouter } from 'expo-router';
 import { Drawer } from 'expo-router/drawer';
 import { ArrowLeft } from 'lucide-react-native';
-import { useColorScheme, View } from 'react-native';
+import { StyleSheet, useColorScheme, View } from 'react-native';
 import 'react-native-get-random-values';
-import { TouchableRipple } from 'react-native-paper';
+import { TouchableRipple, useTheme } from 'react-native-paper';
+import { MD3Colors } from 'react-native-paper/lib/typescript/types';
+import { CustomColors } from '../theme/colors';
+// import { color } from 'react-native-paper';
 
 // export default function RootLayout() {
 // export default function DrawerLayout() {
@@ -31,20 +34,23 @@ export default function DrawerLayout() {
   const router = useRouter();
   const scheme = useColorScheme(); // light | dark
 
+  const { colors } = useTheme();
+  const styles = makeStyles(colors as CustomColors & MD3Colors);
+
+  const rippleColor = colors.onBackground.replace(/[\d.]+\)$/,'0.4)');
+
   return (
     <Drawer defaultStatus="closed"
       screenOptions={({ navigation }) => ({
         headerRight: () => (
           navigation.canGoBack() && (<View
-            style={{
-              overflow: "hidden",
-              borderRadius: 28,
-              marginRight: 4,
-            }}
+            style={styles.backButtonContainer}
           >
             <TouchableRipple
               borderless
-              rippleColor="rgba(0,0,0,.2)"
+              // rippleColor="rgba(0,0,0,.2)"
+              // rippleColor={`rgba(${parseInt(colors.onBackground.slice(1, 3), 16)},${parseInt(colors.onBackground.slice(3, 5), 16)},${parseInt(colors.onBackground.slice(5, 7), 16)},0.2)`}
+              rippleColor={rippleColor}
               onPress={() => router.back()}
             >
               <View
@@ -55,7 +61,7 @@ export default function DrawerLayout() {
                   justifyContent: "center",
                 }}
               >
-                <ArrowLeft size={24} />
+                <ArrowLeft size={24} color={colors.onBackground} />
               </View>
             </TouchableRipple>
           </View>)
@@ -102,6 +108,14 @@ export default function DrawerLayout() {
   );
 }
 
+
+const makeStyles = (colors: CustomColors & MD3Colors) => StyleSheet.create({
+  backButtonContainer: {
+    overflow: "hidden",
+    borderRadius: 28,
+    marginRight: 4,
+  }
+})
 
 // function AppInner() {
 //   const { theme } = useThemeContext();
